cf12b6b91c356a6da0d96547036a278426114694
Shelby Jacobs <shelbyvjacobs@gmail.com>
2019-10-23 16:47:16 -0400

    added results page

--- quiz-page/script.js
+++ quiz-page/script.js
@@ -3,8 +3,6 @@
 // and array of ojects for the questions
 // a variable score set to zero to track the score
 // variable response that is empty to collect the user's responses
-
-
 //MULTIPLE CHOICE; change to radio buttons? silver level
 const questions =[
 	{q: "When was bubble gum invented? \n (a) 1892 \n (b) 1928 \n (c) 1901",
@@ -15,6 +13,9 @@ const questions =[
 	},
 	{q: "How many tons of gum are chewed every year? \n (a) 10,000 \n (b) 1,000,000 \n (c) 100,000",
 	a: "c"
+	},
+	{q: "What came first: chewing gum or bubble gum? \n (a) chewing gum \n (b) bubble gum \n (c) both were invented at the same time",
+	a: "b"
 	}
 ];
 
@@ -23,12 +24,13 @@ let rightOrWrong = document.querySelector("#rightOrWrong");
 const nextButton = document.querySelector(".next");
 const startButton = document.querySelector(".start");
 const mainGame = document.querySelector(".mainGame");
+const resetButton = document.querySelector(".reset");
+const results = document.querySelector(".results");
+let scoreContainer = document.querySelector(".scoreContainer");
 const form = document.querySelector('#form');
 const modal = document.querySelector(".modal");
 const instructions = document.querySelector(".instructions");
-let score = 0;
-
-
+let userScore = 0;
 //start the game
 startButton.addEventListener("click", startGame);
 function startGame () {
@@ -37,72 +39,64 @@ function startGame () {
 	mainGame.classList.remove("hide");
 };
 
-// function askQuestion () {
-// 	for (let i=0; i<questions.length; i++){
-// 		questionSection.innerText = questions[i].q;
-// 		//for some reason on the last question is showing.
-// 		//all of the questions just simultaneously/very rapidly .innerText-ing, and I only see the last one because that's where it stops...
-// 		//console shows all of the questions!
-// 	}
-// }
-// askQuestion();
+function shareResults() {
+	mainGame.classList.add("hide");
+	nextButton.classList.add("hide");
+	results.classList.remove("hide");
+	console.log(scoreContainer);
+	scoreContainer.classList.remove("hide");
+	scoreContainer.innerText = "You answered " + userScore + "/" + questions.length + " correctly. Good job!";
+	resetButton.classList.remove("hide");
+};
+
+resetButton.addEventListener("click", resetGame);
+function resetGame () {
+	location.reload();
+};
 
 i=0;
 
-function nextQuestion (){
-	questionSection.innerText = questions[i].q;
-	nextButton.addEventListener("click", function(evt){
-		evt.preventDefault();
-		nextButton.classList.remove("hide");
-		i += 1
-		input.reset(); //to clear the input box; console says this is not a function
-		console.log(i)
-		checkAnswer(); //DID I JUST MAKE A RECURSIVE FUNCTION?!
-		//Clear the input box
-	})
-}
-
-nextQuestion();
-
 function checkAnswer () {
 	// console.log(questions[index].q)
-	// questionSection.innerText = questions[i].q;
+	questionSection.innerText = questions[i].q;
 	rightOrWrong.innerText = "";
 	form.addEventListener("submit", function(evt){
 		evt.preventDefault();
 		let response = evt.target.querySelector("#response").value;
 		console.log(response); //logs 3 times for each answer
 		if (response.toLowerCase() == questions[i].a){
-			score++
-			rightOrWrong.style.color = "green"
+			userScore++
+			rightOrWrong.style.color = "#6eff7a"
 			rightOrWrong.style.fontSize = "40px";
 			rightOrWrong.innerText = "Correct!"
-			// nextButton.classList.remove("hide");
+			nextButton.classList.remove("hide");
 			//move on to next question
 		} else if (response == ""){
 			rightOrWrong.style.color = "black"
 			rightOrWrong.style.fontSize = "20px";
 			rightOrWrong.innerText = "Please enter an answer!"
 		} else {
-			rightOrWrong.style.color = "red"
+			rightOrWrong.style.color = "#ff6e6e"
 			rightOrWrong.style.fontSize = "40px";
 			rightOrWrong.innerText = "Incorrect!"
-			// nextButton.classList.remove("hide");
+			nextButton.classList.remove("hide");
 		}
-	});
-	
-	// //next button
-	// nextButton.addEventListener("click", function(evt){
-	// 	evt.preventDefault();
-	// 	i += 1
-	// 	// form.reset(); //console says this is not a function
-	// 	console.log(i)
-	// 	checkAnswer(); //DID I JUST MAKE A RECURSIVE FUNCTION?!
-	// 	//Clear the input box
-	// })
+	})
+	//next button
+	nextButton.addEventListener("click", function(evt){
+		evt.preventDefault();
+		i += 1
+		if (i < questions.length - 1){
+			checkAnswer();
+			console.log(i);
+		} else {
+			console.log(i);
+			console.log("Results page!")
+			shareResults();
+		}
+	})
 };
-
-// checkAnswer();
+checkAnswer();
 
 //instructions modal
 instructions.addEventListener("click", function(evt){
@@ -117,14 +111,3 @@ window.addEventListener("click", function(evt) {
 		modal.style.display = "none";
 	}
 });
-
-
-// nextButton.addEventListener("click", function(evt){
-// 	i+=1;
-// })
-
-// trigger this only when all of the questions have been answered.
-// if (questions[i] == questions.length) {
-// console.log("You got " + score + "/" + questions.length + " correct!");
-// };
-
